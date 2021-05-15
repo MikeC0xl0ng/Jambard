f1bd945fc8f4f8d051210a719f5a6084154363a7
unknown <bodliev.mykola@gmail.com>
2021-05-15 11:10:35 +0200

    Server side update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const io = new Server(server);
 const fs = require("fs");
 
 var utenti = {};
+var nextID = 0; 
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "\\jamboard.html");
@@ -15,6 +16,7 @@ app.get("/", function(req, res){
 io.on("connection", function(socket){
 
   var utente;
+  socket.currentLine = undefined;
 
   socket.on("imposta_utente", (nome) => {
     utente = nome;
@@ -22,27 +24,51 @@ io.on("connection", function(socket){
     console.log("Utente " + utente + " è entrato");    
   });
 
-  socket.on("aggiorna_linea", function(punto){
-    var id = Object.keys(utenti[utente]).length - 1;
-    //console.log("Linea continuata con id: " + id);
-    utenti[utente][id].punti.push({ "x": punto.x, "y": punto.y });
-    console.log("Utente " + utente + " ha le seguente linee:");
-    console.log(utenti[utente]);
-    console.log(utenti[utente][id].punto);
-    var o = { "punto": punto, "ultimo_punto": utenti[utente][id].punto, "utente": utente };
-    io.emit("aggiungi_punto", o);
+  // socket.on("aggiorna_linea", function(punto){
+  //   var id = Object.keys(utenti[utente]).length - 1;
+  //   //console.log("Linea continuata con id: " + id);
+  //   utenti[utente][id].punti.push({ "x": punto.x, "y": punto.y });
+  //   console.log("Utente " + utente + " ha le seguente linee:");
+  //   console.log(utenti[utente]);
+  //   console.log(utenti[utente][id].punto);
+  //   var o = { "punto": punto, "ultimo_punto": utenti[utente][id].punto, "utente": utente };
+  //   // manda punto e il suo id
+  //   io.emit("aggiungi_punto", o);
+  // });
+
+  // socket.on("crea_linea", (punto) => {
+  //   var id = Object.keys(utenti[utente]).length;
+  //   console.log("Linea creata con id: " + id);
+  //   var linea = { "punti": [{"x": punto.x, "y": punto.y }], "color": punto.col };
+  //   utenti[utente][id] = linea;
+  //   // guarda è stata aggiunta una linea
+  //   var o = {"punto": punto, "ultimo_punto": { "x": punto.x, "y": punto.y }, "utente": utente };
+  //   io.emit("aggiungi_punto", o);
+  //   io.emit("crea_nuova_linea", linea);
+  // });
+
+  socket.on("aggiungi_punto", (punto) => {
+    if(socket.currentLine == undefined){
+      socket.currentLine = { "id": nextID, "linea": [], "colore": punto.color };
+      nextID = nextID + 1;
+    }
+    aggiungiPuntoAllaLinea(punto, socket.currentLine.linea);
   });
 
-  socket.on("crea_linea", (punto) => {
-    var id = Object.keys(utenti[utente]).length;
-    console.log("Linea creata con id: " + id);
-    var linea = { "punti": [{"x": punto.x, "y": punto.y }], "color": punto.col };
-    utenti[utente][id] = linea;
-    // guarda è stata aggiunta una linea
-    var o = {"punto": punto, "ultimo_punto": { "x": punto.x, "y": punto.y }, "utente": utente };
-    io.emit("aggiungi_punto", o);
-    io.emit("crea_nuova_linea", linea);
+  socket.on("linea_terminata", (punto) => {
+    aggiungiPuntoAllaLinea(punto, socket.currentLine.linea);
+    socket.currentLine = undefined;
   });
+
+  function aggiungiPuntoAllaLinea(punto, linea){
+    linea.push(punto);
+    io.emit("aggiungi_punto", linea.punto);
+  }
+
+  // clickdown - evento
+  // mouse up - linea terminata
+  // il server quando riceve una linea nuova la crea e dà un id e comunica (sempre) la stessa cosa: nuovo punto di questo utente e questa linea
+  // il cliente si chiede "Ce l'ho?" il push / creano la linea
 
   //socket.on("cambia_colore", function(colore){});
 
