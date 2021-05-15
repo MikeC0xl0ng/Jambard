238a6aade547dc4a79992a4b773dda890e1cebac
unknown <bodliev.mykola@gmail.com>
2021-05-15 12:01:06 +0200

    Canvas drawing completely working
    Updating canvas on mousemove by adding points on it

--- server.js
+++ server.js
@@ -24,29 +24,6 @@ io.on("connection", function(socket){
     console.log("Utente " + utente + " è entrato");    
   });
 
-  // socket.on("aggiorna_linea", function(punto){
-  //   var id = Object.keys(utenti[utente]).length - 1;
-  //   //console.log("Linea continuata con id: " + id);
-  //   utenti[utente][id].punti.push({ "x": punto.x, "y": punto.y });
-  //   console.log("Utente " + utente + " ha le seguente linee:");
-  //   console.log(utenti[utente]);
-  //   console.log(utenti[utente][id].punto);
-  //   var o = { "punto": punto, "ultimo_punto": utenti[utente][id].punto, "utente": utente };
-  //   // manda punto e il suo id
-  //   io.emit("aggiungi_punto", o);
-  // });
-
-  // socket.on("crea_linea", (punto) => {
-  //   var id = Object.keys(utenti[utente]).length;
-  //   console.log("Linea creata con id: " + id);
-  //   var linea = { "punti": [{"x": punto.x, "y": punto.y }], "color": punto.col };
-  //   utenti[utente][id] = linea;
-  //   // guarda è stata aggiunta una linea
-  //   var o = {"punto": punto, "ultimo_punto": { "x": punto.x, "y": punto.y }, "utente": utente };
-  //   io.emit("aggiungi_punto", o);
-  //   io.emit("crea_nuova_linea", linea);
-  // });
-
   socket.on("aggiungi_punto", (punto) => {
     if(socket.currentLine == undefined){
       socket.currentLine = { "id": nextID, "linea": [], "colore": punto.color };
@@ -69,13 +46,6 @@ io.on("connection", function(socket){
     }
     io.emit("aggiungi_punto", punti);
   }
-
-  // clickdown - evento
-  // mouse up - linea terminata
-  // il server quando riceve una linea nuova la crea e dà un id e comunica (sempre) la stessa cosa: nuovo punto di questo utente e questa linea
-  // il cliente si chiede "Ce l'ho?" il push / creano la linea
-
-  //socket.on("cambia_colore", function(colore){});
 
   socket.on("disconnect", function(){
     console.log("qualcuno si è disconnesso");
